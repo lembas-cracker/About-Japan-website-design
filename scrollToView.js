e5855c134b7cb135bcf8aa8e8c869bacc42b6a62
Ksenia <agalaksu111@gmail.com>
2018-04-12 01:29:15 +0300

    Included updated version of scrollToView.js

--- scrollToView.js
+++ scrollToView.js
@@ -1,97 +1,10 @@
-//Drawer-1//
-$('.click-handler-1').click(function () {
-  var offset = $(this).offset();
-  offset.top -= 860;
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-2').click(function () {
-  var offset = $(this).offset();
-  offset.top -= 140;
+//Drawer-1
+$('[scroll-to]').click(function () {
+  var whereToScroll = $(this).attr('scroll-to');
+  var titleOffset = $(whereToScroll).offset().top;
+  var drawerOffset = $(".drawer").offset().top;
 
   $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-3').click(function () {
-  var offset = $(this).offset();
-  offset.top += 420;
-
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-4').click(function () {
-  var offset = $(this).offset();
-  offset.top += 1030;
-
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-5').click(function () {
-  var offset = $(this).offset();
-  offset.top += 2020;
-
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-6').click(function () {
-  var offset = $(this).offset();
-  offset.top += 3100;
-
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-
-//Drawer-2//
-$('.click-handler-7').click(function () {
-  var offset = $(this).offset();
-  offset.top -= 1020;
-
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-8').click(function () {
-  var offset = $(this).offset();
-  offset.top += 330;
-
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-9').click(function () {
-  var offset = $(this).offset();
-  offset.top += 1320;
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-
-//Drawer-3//
-
-$('.click-handler-10').click(function () {
-  var offset = $(this).offset();
-  offset.top -= 985;
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-11').click(function () {
-  var offset = $(this).offset();
-  offset.top += 440;
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
-});
-$('.click-handler-12').click(function () {
-  var offset = $(this).offset();
-  offset.top += 1400;
-  $('.drawer').animate({
-    scrollTop: offset.top,
-});
+    scrollTop: titleOffset - drawerOffset
+  })
 });
